a9b653dfd991eb30a73ce0d4a3970d27ad97ab55
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-01 01:14:57 +0200

    add lowest common ancestor of BT py

---
+++ python/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+# Output: 5
+# Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Time: O(n)
+# Space: O(h) - height
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        
+        # If p and q found in left and right subtree of this node, then this node is LCA
+        if left and right:
+            return root
+        # Else return the node which returned a node from it's subtree such that one of it's ancestor will be LCA
+        else:
+            return left or right
