c8deb827d7e01428db57019c50372b08270883b2
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-02 23:47:19 +0200

    add insert into a binary search tree py

---
+++ python/0701-insert-into-a-binary-search-tree.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Input: root = [4,2,7,1,3], val = 5
+# Output: [4,2,7,1,3,5]
+
+# Input: root = [40,20,60,10,30,50,70], val = 25
+# Output: [40,20,60,10,30,50,70,null,null,25]
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
