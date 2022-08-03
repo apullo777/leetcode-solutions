c994e6bdc467b23fe98568b2e7cc2d9d2a0b5230
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-04 00:33:35 +0200

    add mid of a LS py

---
+++ python/0876-middle-of-the-linked-list.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
