f0fdf7f2efb33c7ad560f0faa51cf29acbaca85d
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-03 22:23:59 +0200

    add search in a binary tree py

---
+++ python/0700-search-in-a-binary-tree.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+# Input: root = [4,2,7,1,3], val = 2
+# Output: [2,1,3]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root and val < root.val:
+            return self.searchBST(root.left, val)
+        elif root and val > root.val:
+            return self.searchBST(root.right, val)
+        return root
