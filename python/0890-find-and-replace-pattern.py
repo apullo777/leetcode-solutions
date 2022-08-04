06afdf35f2de426398bdf10fb3e17ee9cc96cfc6
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-05 00:48:52 +0200

    add find and replace pattern py

--- python/0890-find-and-replace-pattern.py
+++ python/0890-find-and-replace-pattern.py
@@ -1,5 +1,10 @@
 # https://leetcode.com/problems/find-and-replace-pattern/
 
+# Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+# Output: ["mee","aqq"]
+# Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}. 
+# "ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation, since a and b map to the same letter.
+
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
         def translate(word):
