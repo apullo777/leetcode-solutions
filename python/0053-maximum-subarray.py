ee5ae4f4f53aa7436f5648c32773cfabe55a9240
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-11 02:08:27 +0200

    add divide & conquer solutions

--- python/0053-maximum-subarray.py
+++ python/0053-maximum-subarray.py
@@ -32,3 +32,45 @@ class Solution:
             curSum = max(num, curSum + num)
             maxSum = max(curSum, maxSum)
         return maxSum
+
+# Divide and Conquer 
+# Recurrence relation: T(N) = 2T(N/2) + O(N)
+# Time: O(n log n)
+# Space: O(log n) 
+
+# divide the array into sub-problems on the left and right halves 
+# and then combine these results on the way back up to find the maximum subarray sum.       
+
+class Solution:
+    def maxSubArray(self, nums):
+        def maxSubArray(A, L, R):
+            if L > R: return -inf
+            mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
+            for i in range(mid-1, L-1, -1):
+                left_sum = max(left_sum, cur_sum := cur_sum + A[i])
+            cur_sum = 0
+            for i in range(mid+1, R+1):
+                right_sum = max(right_sum, cur_sum := cur_sum + A[i])
+            return max(maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), left_sum + A[mid] + right_sum)
+        return maxSubArray(nums, 0, len(nums)-1)
+
+# Optimized Divide & Conquer
+# Recurrence relation: T(N) = 2T(N/2) + O(1)
+# Time: O(n)
+# Space: O(n)
+
+# The O(N) term in the recurrence relation of previous solution was due to computation of max sum subarray involving nums[mid] in each recursion.
+# But we can reduce that term to O(1) if we precompute it. 
+
+class Solution:
+    def maxSubArray(self, nums):
+        # pre[i] denotes the maximum subarray ending at i
+        # suf[i] denotes the maximum subarray starting at i
+        pre, suf = [*nums], [*nums]
+        for i in range(1, len(nums)):       pre[i] += max(0, pre[i-1])
+        for i in range(len(nums)-2,-1,-1):  suf[i] += max(0, suf[i+1])
+        def maxSubArray(A, L, R):
+            if L == R: return A[L]
+            mid = (L + R) // 2
+            return max(maxSubArray(A, L, mid), maxSubArray(A, mid+1, R), pre[mid] + suf[mid+1])
+        return maxSubArray(nums, 0, len(nums)-1)
