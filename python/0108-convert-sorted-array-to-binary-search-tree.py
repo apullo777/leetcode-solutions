f91f90b2490f5ed7149dcbb6111b202b7fa1b395
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-22 12:48:04 +0200

    add convert sorted array to BST py

---
+++ python/0108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree.py/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Input: nums = [-10,-3,0,5,9]
+# Output: [0,-3,9,-10,null,5]
+# Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+# Input: nums = [1,3]
+# Output: [3,1]
+# Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        
+        return root
