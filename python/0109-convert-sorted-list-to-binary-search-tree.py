03f8686b466ce9b405343c62e5889df81f1a47f4
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-23 11:35:49 +0200

    add convert sorted list to BST py

---
+++ python/0109-convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Input: head = [-10,-3,0,5,9]
+# Output: [0,-3,9,-10,null,5]
+# Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+
+# Input: head = []
+# Output: []
+
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre = slow
+            slow = slow.next
+            fast = fast.next.next
+        pre.next = None
+        root = TreeNode(slow.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(slow.next)
+        return root
