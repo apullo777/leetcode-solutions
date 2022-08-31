f66b4ea11fff9089ec02ee0f6776688623ba2331
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-31 12:02:27 +0200

    add unique morse code words py

---
+++ python/0804-unique-morse-code-words.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/unique-morse-code-words/
+
+# Input: words = ["gin","zen","gig","msg"]
+# Output: 2
+# Explanation: The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+# There are 2 different transformations: "--...-." and "--...--.".
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        alphabet = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        ans = []
+        for word in words:
+            ans.append("".join(alphabet[ord(char) - ord("a")] for char in word))
+        return len(set(ans))
